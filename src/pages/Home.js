776b47b8185d073d053048058f1e432a7cf5e795
Seth Jones <setheryjones32@yahoo.com>
2025-05-22 22:57:33 -0400

    new button test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button } from '@headlessui/react';
+
 
 
 
@@ -10,7 +12,7 @@ function Home() {
     <div className="App" style = {{textAlign: 'center', marginTop: '100px'}}>
       <h1> Spotify Playlist App</h1>
       <p>Login with your Spotify account to begin</p>
-      <button onClick = {handleLogin} style = {{ padding: '12px 20px', fontSize: '16px' }}>
+      <button onClick = {handleLogin} className="rounded bg-sky-600 px-4 py-2 text-sm text-white data-active:bg-sky-700 data-hover:bg-sky-500">
         Log in with Spotify
       </button>
     </div>
